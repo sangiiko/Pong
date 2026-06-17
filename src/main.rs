f6eab76bf264087ad6973c0a71e302ae9854b59a
sangiiko <xuannguyen.030498@gmail.com>
2026-06-17 15:25:59 +0700

    Rendered a square

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new().add_plugins(DefaultPlugins).add_systems(Startup,setup).run();
+}
+
+fn setup(mut commands: Commands){
+   commands.spawn(Camera2d);
+
+   commands.spawn(Sprite {
+       custom_size: Some(Vec2::new(100.0,100.0)),
+       color: Color::srgb(1.0,1.0,1.0),
+       ..default()
+   });
 }
